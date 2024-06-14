80c277af7cb208fccbd9a3bd75888c885c7f890b
sushanth-0 <102034708+sushanth-0@users.noreply.github.com>
2024-06-14 19:43:28 -0400

    app.py updated

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()  # Initialize the client app
-    app.run(host='0.0.0.0', port=8080)  # Run the Flask app
+    app.run(host='0.0.0.0', port=8081)  # Run the Flask app
